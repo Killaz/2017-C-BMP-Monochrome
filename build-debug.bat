dbff185a19d0bd51a46635c0fee44017b71bdd5c
Kanootoko <kanootoko@gmail.com>
2018-01-04 14:46:43 +0300

    Changed compiling options a bit.

--- build-debug.bat
+++ build-debug.bat
@@ -1,3 +1,3 @@
 @echo off
 cmake -G "MinGW Makefiles" -DDEBUG=1 CMakeLists.txt
-mingw32-make
+cmake --build .
--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 cmake -G "MinGW Makefiles" -DDEBUG=0 CMakeLists.txt
-mingw32-make
+cmake --build .
